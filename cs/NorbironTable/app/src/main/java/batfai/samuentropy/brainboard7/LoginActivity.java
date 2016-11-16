b27a9b41d5e59d88f5cef3f901cadeaf7e5b0bb6
Benyuss <egokiraly@gmail.com>
2016-11-16 12:44:46 +0100

    resource leak fixed.

--- cs/NorbironTable/app/src/main/java/batfai/samuentropy/brainboard7/LoginActivity.java
+++ cs/NorbironTable/app/src/main/java/batfai/samuentropy/brainboard7/LoginActivity.java
@@ -1,56 +1,58 @@
- package batfai.samuentropy.brainboard7;
+package batfai.samuentropy.brainboard7;
 
- import android.app.ProgressDialog;
- import android.content.Intent;
- import android.os.Bundle;
- import android.support.annotation.NonNull;
- import android.support.v7.app.AppCompatActivity;
- import android.text.TextUtils;
- import android.util.Log;
- import android.view.View;
- import android.widget.Button;
- import android.widget.EditText;
- import android.widget.TextView;
- import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
- import com.google.android.gms.auth.api.Auth;
- import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
- import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
- import com.google.android.gms.auth.api.signin.GoogleSignInResult;
- import com.google.android.gms.common.ConnectionResult;
- import com.google.android.gms.common.SignInButton;
- import com.google.android.gms.common.api.GoogleApiClient;
- import com.google.android.gms.common.api.OptionalPendingResult;
- import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 // import com.google.android.gms.iid.InstanceID;
 
- public class LoginActivity extends AppCompatActivity implements
- GoogleApiClient.OnConnectionFailedListener, View.OnClickListener
- {
+public class LoginActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
     private EditText emailField;
     private EditText passwordField;
     private Button loginButton;
     private Button registerButton;
 
-     private boolean signedIn = false;
+    private boolean signedIn = false;
 
-     private static final String TAG = "LoginActivity";
-     private static final int RC_SIGN_IN = 9001;
+    private static final String TAG = "LoginActivity";
+    private static final int RC_SIGN_IN = 9001;
 
-     private GoogleApiClient mGoogleApiClient;
-     private TextView mStatusTextView;
-     private ProgressDialog mProgressDialog;
+    private GoogleApiClient mGoogleApiClient;
+    private TextView mStatusTextView;
+    private ProgressDialog mProgressDialog;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainlogin);
-        emailField=(EditText)(findViewById(R.id.emailText));
-        passwordField=(EditText) findViewById((R.id.passwordText));
+        emailField = (EditText) (findViewById(R.id.emailText));
+        passwordField = (EditText) findViewById((R.id.passwordText));
 
-        loginButton = (Button)findViewById(R.id.loginButton);
-        registerButton = (Button)findViewById(R.id.registerButton);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.plain_toolbar);
+        setSupportActionBar(toolbar);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        registerButton = (Button) findViewById(R.id.registerButton);
 
      /*   Intent intent = new Intent(LoginActivity.this,MainActivity.class);
         Bundle mBundle = new Bundle();
@@ -58,19 +60,15 @@
         intent.putExtras(mBundle);
         startActivity(intent);
 */
-        loginButton.setOnClickListener(new View.OnClickListener()
-        {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 signIn(emailField.getText().toString(), passwordField.getText().toString());
             }
         });
-        registerButton.setOnClickListener(new View.OnClickListener()
-        {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 createAccount(emailField.getText().toString(), passwordField.getText().toString());
             }
         });
@@ -81,10 +79,9 @@
                 .build();
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this , this)
+                .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-
 
 
         SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
@@ -97,104 +94,92 @@
 
     }
 
-     @Override
-     public void onStart()
-     {
-         super.onStart();
+    @Override
+    public void onStart() {
+        super.onStart();
 
-         OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
-         if (opr.isDone())
-         {
-             // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
-             // and the GoogleSignInResult will be available instantly.
-             Log.d(TAG, "Got cached sign-in");
-             GoogleSignInResult result = opr.get();
-             handleSignInResult(result);
-         }
-         else
-         {
-             // If the user has not previously signed in on this device or the sign-in has expired,
-             // this asynchronous branch will attempt to sign in the user silently.  Cross-device
-             // single sign-on will occur in this branch.
-             showProgressDialog();
-             opr.setResultCallback(new ResultCallback<GoogleSignInResult>()
-             {
-                 @Override
-                 public void onResult(@NonNull GoogleSignInResult googleSignInResult)
-                 {
-                     hideProgressDialog();
-                     handleSignInResult(googleSignInResult);
-                 }
-             });
-         }
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            Log.d(TAG, "Got cached sign-in");
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            showProgressDialog();
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+                    hideProgressDialog();
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
 
 
-     }
+    }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == RC_SIGN_IN)
-        {
+        if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleSignInResult(result);
         }
     }
 
-     private void handleSignInResult(GoogleSignInResult result)
-     {
-         Log.d(TAG, "handleSignInResult:" + result.isSuccess());
-         if (result.isSuccess())
-         {
-             signedIn = true;
-             // Signed in successfully, show authenticated UI.
-             GoogleSignInAccount acct = result.getSignInAccount();
-             assert acct != null;
-             mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
-             updateUI(signedIn);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mProgressDialog.dismiss();
+    }
 
-           //  String iid = InstanceID.getInstance(context).getId();
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            signedIn = true;
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            assert acct != null;
+            mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            updateUI(signedIn);
+
+            //  String iid = InstanceID.getInstance(context).getId();
 
 
-         }
-         else
-         {
-             updateUI(signedIn);
-         }
-     }
+        } else {
+            updateUI(signedIn);
+        }
+    }
 
-    private void createAccount(String email, String password)
-    {
+    private void createAccount(String email, String password) {
         Log.d(TAG, "createAccount:" + email);
-        if(TextUtils.isEmpty(email))
-        {
-            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_LONG).show();
             return;
         }
 
-        if(TextUtils.isEmpty(password))
-        {
-            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_LONG).show();
 
         }
 
     }
 
-    private void signIn(String email, String password)
-    {
+    private void signIn(String email, String password) {
         Log.d(TAG, "signIn:" + email);
 
-        if(TextUtils.isEmpty(email))
-        {
-            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_LONG).show();
             return;
         }
 
-        if(TextUtils.isEmpty(password))
-        {
-            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_LONG).show();
         }
 
         //if the email and password are not empty
@@ -202,66 +187,54 @@
 
     }
 
-     private void signIn()
-     {
-         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-         startActivityForResult(signInIntent, RC_SIGN_IN);
-     }
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
 
-     private void showProgressDialog()
-     {
-         if (mProgressDialog == null)
-         {
-             mProgressDialog = new ProgressDialog(this);
-             mProgressDialog.setMessage(getString(R.string.loading));
-             mProgressDialog.setIndeterminate(true);
-         }
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
 
-         mProgressDialog.show();
-     }
+        mProgressDialog.show();
+    }
 
-     private void hideProgressDialog()
-     {
-         if (mProgressDialog != null && mProgressDialog.isShowing())
-         {
-             mProgressDialog.hide();
-         }
-     }
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
 
-     private void updateUI(boolean signedIn)
-     {
-         if (signedIn)
-         {
-             findViewById(R.id.signedIn).setVisibility(View.VISIBLE);
-             findViewById(R.id.signInAndSignUpButton).setVisibility(View.GONE);
-             start();
-         }
-     }
+    private void updateUI(boolean signedIn) {
+        if (signedIn) {
+            findViewById(R.id.signedIn).setVisibility(View.VISIBLE);
+            findViewById(R.id.signInAndSignUpButton).setVisibility(View.GONE);
+            start();
+        }
+    }
 
-     @Override
-     public void onClick(View view)
-     {
-         switch (view.getId())
-         {
-             case R.id.sign_in_button:
-             {
-                 signIn();
-                 break;
-             }
-         }
-     }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sign_in_button: {
+                signIn();
+                break;
+            }
+        }
+    }
 
-     @Override
-     public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
-     {
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
-     }
+    }
 
-     public void start()
-     {
-         Intent intent = new Intent();
-         intent.setClass(this, NeuronGameActivity.class);
-         this.startActivity(intent);
-     }
- }
+    public void start() {
+        Intent intent = new Intent();
+        intent.setClass(this, NeuronGameActivity.class);
+        this.startActivity(intent);
+    }
+}
